fbb7093467a079809601b6fc2fe0628bec0ff8a7
nithen-pv <pvnithen14@gmail.com>
2021-06-05 11:33:55 +0530

    background theme and appbar loaded

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import {AppBar,Typography} from '@material-ui/core'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div >
+      <AppBar position="static" color="inherit">
+        <Typography variant="h4" align="center">Video Chat App</Typography>
+      </AppBar>
     </div>
   );
 }
